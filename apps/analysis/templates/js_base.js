5db831bb153669a835c85f06f13d6e1aac27f098
murphy-mtt <zd_wangfenglin@163.com>
2020-02-13 16:40:27 +0800

    nothing useful

---
+++ apps/analysis/templates/js_base.js
@@ -0,0 +1,12 @@
+var SCATTER01 = [
+    "SCATTER01",
+    {SCATTERRadius: 90,
+        innerCircleSize: 1,
+        outerCircleSize: 3,
+        innerCircleColor: "#F26223",
+        outerCircleColor: "#FF6666",
+        random_data: 40},
+    [
+        {{ content }}
+    ]
+];
--- apps/analysis/views.py
+++ apps/analysis/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views.generic import View, TemplateView, DetailView
 
-# Create your views here.
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'analysis/index.html', {})
+
+
+class ProjectView(View):
+    def get(self, request):
+        pass

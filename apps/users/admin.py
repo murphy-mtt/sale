8c329b8ab9ca92c3391d4197eaa328a6f292002b
murphy-mtt <zd_wangfenglin@163.com>
2020-02-16 10:55:54 +0800

    userprofile admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import UserProfile
 
 # Register your models here.
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'is_staff', 'is_active']
